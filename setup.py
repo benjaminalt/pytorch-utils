1686fa83961a9f02aefcc7226adeebee3a4c9d53
Benjamin Alt <benjamin.alt@artiminds.com>
2021-03-04 22:31:16 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pytorch-utils',
+    version='1.0',
+    packages=['pytorch_utils', 'pytorch_utils.test'],
+    url='https://github.com/benjaminalt/pytorch-utils',
+    license='MIT License',
+    author='Benjamin Alt',
+    author_email='benjamin.alt@artiminds.com',
+    description=''
+)
